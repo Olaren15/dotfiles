e6679f660154609234d45ae9307a3156c0fd47e3
Catherine Gilbert <catherine.gilbert15@gmail.com>
2024-02-21 19:16:07 -0500

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,10 @@ end
 # Remove greeting
 set -U fish_greeting
 
+fish_config theme choose "Dracula Official"
+
+fish_add_path ~/.local/bin
+
 # Use wayland when possible
 set -U SDL_VIDEODRIVER wayland,x11
 set -U ELECTRON_OZONE_PLATFORM_HINT wayland
