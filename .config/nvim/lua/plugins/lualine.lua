93c72f2d44fa8367359f13da2f2bd77426f173f3
Catherine Gilbert <catherine.gilbert15@gmail.com>
2024-05-31 16:32:35 -0400

    updated config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
     event = "VeryLazy",
     opts = {
         options = {
-            theme = "grubox",
+            theme = "auto",
             component_separators = "|",
             section_separators = "",
         },
