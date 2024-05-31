4942fbcae70854a8b80d6df4b5e49c733bdf7bf6
Catherine Gilbert <catherine.gilbert15@gmail.com>
2024-05-30 20:54:13 -0400

    Changed vim theme to gruvbox

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,9 @@
 return {
-    "Mofiqul/dracula.nvim",
+    "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme("dracula")
+        vim.o.background = "dark"
+        vim.cmd.colorscheme("gruvbox")
     end
 }
