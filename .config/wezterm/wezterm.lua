9a4675662fab474ba96d80f1b1c1d703baee3dac
Catherine Gilbert <catherine.gilbert15@gmail.com>
2024-10-18 18:38:09 -0400

    updated wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Gruvbox dark, medium (base16)"
 
 config.use_fancy_tab_bar = false
--- config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = true
 
 config.window_padding = {
 	left = 0,
